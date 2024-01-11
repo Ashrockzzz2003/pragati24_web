4ee472c1ab24883dc13c36955b162c74de55a41e
Ashrockzzz2003 <ashrockzzz2003@gmail.com>
2024-01-11 14:33:07 +0530

    fontSize increased title

--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
 "use client";
 
-import Footer from "@/components/Footer";
 import NavBar from "@/components/NavBar";
 import { useEffect, useRef, useState } from "react";
 
@@ -25,14 +24,14 @@ export default function Home() {
         const { docViewer } = instance;
         setVW(instance);
       });
-    })
+    });
   }, [vw]);
 
   return (
     <>
       <NavBar />
       <main className="h-[87vh]">
-        <h1 className="mb-6 pt-8 text-2xl text-lime-50 text-center">Pragati 2024</h1>
+        <h1 className="mb-6 pt-8 text-4xl text-lime-50 text-center">Pragati 2024</h1>
         <div className="w-[90%] h-[78%] m-auto rounded-2xl overflow-visible viewer" ref={viewer}></div>
         {/* <div className="flex flex-col bg-gray-50 bg-opacity-70 rounded-xl p-4 w-fit ml-auto mr-auto my-4">
           <p className="text-sm font-medium text-gray-700 max-w-[256px] text-center">{"Note: The brochure is loading. Please wait a few seconds!"}</p>
